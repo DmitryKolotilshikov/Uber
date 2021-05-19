ff3e7aedda897d978ae0afc226b9508912fb91ce
Dmitry Kolotilshikov <57001255+DmitryKolotilshikov@users.noreply.github.com>
2021-05-19 17:41:24 +0300

    с3

--- js/burger.js
+++ js/burger.js
@@ -2,14 +2,23 @@ window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu');
     const menuItem = document.querySelectorAll('.list_item');
     const burger = document.querySelector('.burger');
-
-    burger.addEventListener('click', () => {
+    const body = document.body;
+    let toggleOverflow = true;
+     
+    burger.addEventListener('click', () => {        
+        if (toggleOverflow) {
+            body.style.overflow = 'hidden';
+        } else {
+            body.style.overflow = 'visible';
+        }              
         burger.classList.toggle('burger_active');
         menu.classList.toggle('menu_active');
+        toggleOverflow = !toggleOverflow;
     });
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
+            body.style.overflow = 'visible';
             burger.classList.toggle('burger_active');
             menu.classList.toggle('menu_active');
         })
